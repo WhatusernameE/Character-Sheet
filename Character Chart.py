9ca0250d9d33a27fe81a3efc0bafa184e5c1ddd1
WhatusernameE <123575484+WhatusernameE@users.noreply.github.com>
2023-03-24 08:03:08 -0600

    Add files via upload

---
+++ Character Chart.py
@@ -0,0 +1,15 @@
+# Write a program in Codeboard that contains an object and uses it. (Such as writing out a object for your favorite
+# character in a game, movie, or TV show)
+
+lotr = {
+    "key": "Value",
+    "total RT": "11.2 hours",
+    "enjoyment": "10/10 would watch again",
+    "characters": "10/10",
+    "fun": 10,
+    "num of movies": ["1 Fellow Ship of the Ring", "2 The Two Towers", "3 The Return of the King."],
+    "years": ["12/9/2001", "12/18/2002", "12/17/2003"]
+}
+print("Total run time:", lotr["total RT"])
+print("Number of movies:", lotr["num of movies"])
+print("What year the movies were made!", lotr["years"])
